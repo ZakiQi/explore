bf30e5f01409ae30cd7108c80d512ba2aaef3d0d
黄泳棋 <yongqi@timesdata.cn>
2021-12-29 17:03:04 +0800

    udpate

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,28 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
 
 
       
+      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 1,
+
+
+
+      b: 2,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
