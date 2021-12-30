57531f0cc4d1540ea4541d1d025714a9a1b11ddd
黄泳棋 <yongqi@timesdata.cn>
2021-12-30 17:44:32 +0800

    ✨特性:

--- .cz-config.js
+++ .cz-config.js
@@ -28,22 +28,17 @@ module.exports = {
     { value: '🔧工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
     { value: '⏪回滚', name: '回滚:    代码回退' }
   ],
-  scopes: [{ name: '模块1' }, { name: '模块2' }, { name: '模块3' }, { name: '模块4' }],
+  scopes: [],
   // 它需要匹配字段类型的值。如。:“修复”
-  /*  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },  */
+  scopeOverrides: {
+    fix: [{ name: 'merge' }, { name: 'style' }, { name: 'e2eTest' }, { name: 'unitTest' }]
+  },
   // 覆盖消息，默认值如下所示
   messages: {
     type: '提交类型:',
-    // scope: '此次提交的影响范围,如数据层、控制层、视图层等,多个可以用 * 代替',
+    // scope: '此次提交的影响范围,如数据层、控制层、视图层等,多个可以用 * 代替: \n',
     // 如果allowcustomscope为true
-    customScope: '此次提交的影响范围,如数据层、控制层、视图层等,多个可以用 * 代替:\n',
+    customScope: '此次提交的影响范围,如数据层、视图层等,多个可以用 * 代替:\n',
     subject: '短说明:\n',
     body: '长说明，使用"|"换行(可选)：\n',
     breaking: '非兼容性说明 (可选):\n',
