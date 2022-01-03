40cdbe74a229070b9df4c05853e8d09af2b8bf72
黄泳棋 <yongqi@timesdata.cn>
2022-01-03 10:04:27 +0800

    ✨新增: ✨新增: ✨新增: ✨新增: 新增commit stylelint规则校验

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,16 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 1,
+
+      b: 2
+    }
+  }
+}
+</script>
+<style lang="scss"></style>
